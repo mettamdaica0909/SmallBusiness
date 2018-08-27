79f8447e8afda5670982a3269c35116837550e7c
Duy Le <duyld1@fsoft.com.vn>
2018-08-27 13:45:55 +0700

    init project

---
+++ SmallBusiness/SmallBusiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmallBusiness
+//
+//  Created by Duy Le on 8/27/18.
+//  Copyright © 2018 Duy Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
