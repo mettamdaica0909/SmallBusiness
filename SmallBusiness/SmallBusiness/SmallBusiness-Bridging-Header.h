ffaa9ba3e6274206e94014aa8ebbc84d64c639d7
Duy Le <duyld0909@gma>
2018-09-18 10:15:53 +0700

    update

---
+++ SmallBusiness/SmallBusiness/SmallBusiness-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SmallBusiness-Bridging-Header.h
+//  SmallBusiness
+//
+//  Created by Duy Le on 9/17/18.
+//  Copyright © 2018 Duy Le. All rights reserved.
+//
+
+#ifndef SmallBusiness_Bridging_Header_h
+#define SmallBusiness_Bridging_Header_h
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <SDWebImage/SDImageCache.h>
+
+#endif /* SmallBusiness_Bridging_Header_h */
